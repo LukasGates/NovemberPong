bc8c5e14f4ad04c12f34265cf0fbe61ecbc47735
Lukas <lukas.gates@elev.ga.ntig.se>
2020-11-18 10:47:14 +0100

    pong game continues

---
+++ Pong/Paddle.cs
@@ -0,0 +1,10 @@
+using System;
+using Raylib_cs;
+namespace Pong
+{
+    public class Paddle
+    {
+        public float x;
+        public float y;
+    }
+}
---
+++ Pong/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Raylib_cs;
+namespace Pong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Make spicy pong game.
+            Raylib.InitWindow(800, 600, "Pong");
+            Raylib.SetTargetFPS(60);
+
+            float xPos = 10, yPos = 275;
+
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.BEIGE);
+
+                Raylib.DrawRectangle((int) xPos, (int) yPos, 20, 50, Color.BLACK);
+
+                Raylib.EndDrawing();
+            }
+
+
+            
+
+
+        }
+    }
+}
